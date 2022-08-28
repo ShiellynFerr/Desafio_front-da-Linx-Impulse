f412089a3275025a61210aa9c1663c49264a5430
Shiellyn Bruna Ferreira <shiellynferrer@gmail.com>
2022-08-28 15:44:08 -0300

    arquivos inicias do projeto vite

---
+++ linx/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Header from './assets/components/header'
+
+function App() {
+     return (
+      <>
+      <Header/>
+      </>
+     )
+}
+
+export default App
