904986ae15d5f60bf6ff3c0b359c50b2536d4c5d
Shiellyn Bruna Ferreira <shiellynferrer@gmail.com>
2022-08-28 23:15:52 -0300

    adicionando componente footer

---
+++ linx/src/assets/components/footer/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FooterBox, Box} from './style';
+
+export const Footer = () => {
+  return (
+    <FooterBox>
+      <Box>
+      <p>Testando suas habilidades em HTML, CSS e JS.</p>
+      <span>Linx Impulse</span>
+      <span>2019</span>
+      </Box>
+    </FooterBox>
+  )
+}
+
+export default Footer;
---
+++ linx/src/assets/components/footer/style.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const FooterBox = styled.footer `
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+ color: #fff;
+ clip-path: polygon(0 30%, 100% 0%, 100% 100%, 0% 100%);
+ background-color: #422256;
+ height: 250px;
+`
+
+export const Box = styled.div `
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+ margin-top: 5rem;
+ font-size: 14px;
+`
