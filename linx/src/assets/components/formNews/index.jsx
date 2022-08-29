989a333abac8cea68c7d5ee23cf395c8bd191c03
Shiellyn Bruna Ferreira <shiellynferrer@gmail.com>
2022-08-29 18:09:55 -0300

    adicionando componente formNews

---
+++ linx/src/assets/components/formNews/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ButtonForm from '../buttonForm';
+import InputForm from '../inputForm';
+import { Section } from '../part/style';
+import { BoxFormNews, BoxInputs, BoxText } from './style';
+
+ const FormNews = (props) => {
+  return (
+    <BoxFormNews>
+    <Section>
+    <span>
+     <hr/>
+    </span>
+    <h3 texto="Uma seleção especial pra você">Compartilhe a novidade</h3>
+    <span>
+    <hr/>
+    </span>
+ </Section>
+ <BoxText>
+ <h6>Quer que seus amigos também ganhem a lista personalizada deles? Preencha agora!</h6>
+ </BoxText>
+ <BoxInputs>
+  <InputForm label="Nome do seu amigo:" id="nomeAmigo"/>
+  <InputForm label="E-mail" id="emailAmigo"/>
+ </BoxInputs>
+ <ButtonForm texto="Enviar agora"/>
+ </BoxFormNews>
+  )
+}
+
+export default FormNews;
---
+++ linx/src/assets/components/formNews/style.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const BoxFormNews = styled.section `
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+padding: 10px 10px;
+`
+
+export const BoxInputs = styled.div `
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: 3rem;
+margin-bottom:3rem;
+`
+
+export const BoxText = styled.div `
+display: flex;
+justify-content: center;
+align-items: center;
+margin-top: 2rem;
+
+h6 {
+  font-size: 12px;
+  color: #888888;
+}
+`
